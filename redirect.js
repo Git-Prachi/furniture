199e31bf75ac11ca8b3c38ce753ebf20a1f343df
Git-Prachi <141724438+Git-Prachi@users.noreply.github.com>
2023-08-23 20:04:30 +0530

    minor changed done

---
+++ redirect.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Redirect Example</title>
+</head>
+<body>
+
+<script type="text/javascript">
+document.getElementById("redirectButton").onclick = function() {
+    // Change the URL to the desired destination
+    window.location.href = "https://www.google.com/";
+};
+</script>
+
+</body>
+</html>
